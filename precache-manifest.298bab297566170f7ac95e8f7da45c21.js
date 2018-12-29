e19947288aebe12af32c1f8218a80afbdebaaca9
Ahsan-J <ahsan.ahmed99@ymail.com>
2018-12-29 21:26:10 +0500

    Updates

---
+++ precache-manifest.298bab297566170f7ac95e8f7da45c21.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/mastercoin/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b59751f359d610c937e7",
+    "url": "/mastercoin/static/css/main.6d1242e9.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/mastercoin/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "a219e722e270dd3317ec",
+    "url": "/mastercoin/static/js/1.a219e722.chunk.js"
+  },
+  {
+    "revision": "2e1b8e993688dde6607d",
+    "url": "/mastercoin/static/js/runtime~main.2e1b8e99.js"
+  },
+  {
+    "revision": "b59751f359d610c937e7",
+    "url": "/mastercoin/static/js/main.b59751f3.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/mastercoin/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/mastercoin/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/mastercoin/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "a219e722e270dd3317ec",
+    "url": "/mastercoin/static/css/1.01dfab72.chunk.css"
+  },
+  {
+    "revision": "b17d078d914c5a6a452ed0e95279de49",
+    "url": "/mastercoin/index.html"
+  }
+];
