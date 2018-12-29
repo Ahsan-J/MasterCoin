69fed95368ffe88b4a6bd1a6714968a2ff5bc65e
Ahsan-J <ahsan.ahmed99@ymail.com>
2018-12-29 21:30:19 +0500

    Updates

---
+++ precache-manifest.43cc1d4024d2706a3f988acff139cc8c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/MasterCoin/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "5aff6ce09ba674fde577",
+    "url": "/MasterCoin/static/css/main.6d1242e9.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/MasterCoin/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "de02e5665be98b693acd",
+    "url": "/MasterCoin/static/js/1.de02e566.chunk.js"
+  },
+  {
+    "revision": "07dac052b8b1b758f172",
+    "url": "/MasterCoin/static/js/runtime~main.07dac052.js"
+  },
+  {
+    "revision": "5aff6ce09ba674fde577",
+    "url": "/MasterCoin/static/js/main.5aff6ce0.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/MasterCoin/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/MasterCoin/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/MasterCoin/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "de02e5665be98b693acd",
+    "url": "/MasterCoin/static/css/1.6dd7b543.chunk.css"
+  },
+  {
+    "revision": "0973ab7fd362924ee2c713b547c8d443",
+    "url": "/MasterCoin/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/mastercoin/precache-manifest.298bab297566170f7ac95e8f7da45c21.js"
+  "/MasterCoin/precache-manifest.43cc1d4024d2706a3f988acff139cc8c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/mastercoin/index.html", {
+workbox.routing.registerNavigationRoute("/MasterCoin/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
